58d6992067d5c95370c57c463e6ff4459912cb38
titus-cloud <titus.onzere@student.moringaschool.com>
2024-07-13 15:04:56 +0300

    a challenging array map method

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const tutorials = [
+const tutorials=[
   'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
+  'what is the constructor OO pattern?',
+  'implementing blockchain web API',
+  'the test driven development workflow',
+  'what is NaN and how can we check for it',
+  'what is the difference between stopPropagation and preventDefault?',
+  'immutable state and pure functions',
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function convertToTitleCase(title) {
+  return title.split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+}
+
+function titleCased() {
+  return tutorials.map(title => convertToTitleCase(title));
 }
